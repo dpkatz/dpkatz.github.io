46be6f17ddab31966d8eeffc6fefc1f7c2fa4e90
Dan Katz <dpkatz@gmail.com>
2018-04-14 22:41:54 -0400

    Nikola auto commit.
    Nikola version: 7.8.14

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Dan Katz"  # (translatable)
 BLOG_TITLE = "As Simple as Possible..."
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://dpkatz@github.io/"
+SITE_URL = "https://dpkatz.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://haskellml.com/"
