4393492cfd6826fd7e2e63ce00d75ebb3a5114b6
Dan Katz <dpkatz@gmail.com>
2018-05-31 16:39:35 -0400

    Better name for the blog

--- conf.py
+++ conf.py
@@ -18,7 +18,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Dan Katz"  # (translatable)
-BLOG_TITLE = "As Simple as Possible..."
+BLOG_TITLE = "Recursively Boosted"
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://dpkatz.github.io/"
